255ee5f9dd9193c939d9bbea6742ad960d330a99
Seo-RIi <studioSeoRii@gmail.com>
2021-08-22 15:17:10 +0900

    Sync

--- example.js
+++ example.js
@@ -1,8 +1,9 @@
 function getMeal() {
+    let date = new Date()
     return new Promise((resolve, reject) => {
         fetch('https://api.github.com/repos/IASA-Null/mealAction/issues').then(res => res.json()).then(res => {
             for (let i of res) {
-                if (i.title === `I${new Date().getFullYear()}_${new Date().getMonth() + 1}`) {
+                if (i.title === `I${date.getFullYear()}_${date.getMonth() + 1}`) {
                     resolve(JSON.parse(i.body))
                     return
                 }
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const school = neis.createSchool(neis.REGION.INCHOEN, "E100002238", neis.TYPE.HI
 var date = new Date();
 date.setDate(date.getDate() + 3);
 
-school.getMeal(date.getFullYear(), new date.getMonth() + 1).then(mealInfo => {
+school.getMeal(date.getFullYear(), date.getMonth() + 1).then(mealInfo => {
     let res = mealInfo.map(d => {
         return [parseMeal(d.breakfast), parseMeal(d.lunch), parseMeal(d.dinner)]
     }).filter(x => x)
